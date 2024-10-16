c68a0a481bc072c2eecd90065aae67a21b6140c8
Prudence1021 <connorowenbrown@gmail.com>
2024-10-16 08:33:52 -0700

    ch. 2:6

--- main.py
+++ main.py
@@ -4,7 +4,8 @@
 
 import pygame
 from constants import *
-
+from circleshape import *
+from player import *
 
 def main():
 
@@ -12,13 +13,13 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-            
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
